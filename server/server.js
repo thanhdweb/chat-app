26ab18d11aa918ca9589d5b8a03323c43debd964
thanhdweb <thanhdweb@gmail.com>
2025-05-25 21:53:08 +0700

    server update

--- server/server.js
+++ server/server.js
@@ -48,5 +48,9 @@ app.use("/api/messages", messageRouter);
 // Connect to Mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+}
+
+export default server;
